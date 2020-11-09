1820400371d3d4f3d99da8c3611de323906e6603
coder2318 <choriyevfarrux997@gmail.com>
2020-11-10 00:02:34 +0500

    from farruck computer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('chat_id')->unique();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('language_code');
+            $table->integer('step')->default(0);
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
             $table->timestamps();
         });
     }
